30c30c612054a5adf1657c7e4b680bfb77a3fe75
deepak <deepakmehra7830@gmail.com>
2024-12-24 12:25:51 +0530

    ecommerce deta moddling prac

---
+++ ecommerce/product.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+import { Category } from './catogery.model'
+
+const produstSchema = new mongoose.Schema( {
+    discription: {
+        type: String,
+        requried: true
+    },
+    name:{
+        type: String,
+        requried: true
+    },
+    productImg:{
+        type:String
+    },
+    price: {
+        type:Number,
+        default: 0
+    },
+    stock:{
+        type:Number,
+        default: 0
+    },
+    Category: {
+        type: mongoose.Schema.Type.ObjectId,
+        ref: "Category",
+        required:true
+    },
+    owner: {
+        type: mongoose.Schema.Type.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+
+export const Product = mongoose.model("Product", produstSchema)
