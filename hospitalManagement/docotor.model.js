d5aa7a0e9d4bb797625486b08792fde61aae46a0
deepak <deepakmehra7830@gmail.com>
2024-12-24 14:51:51 +0530

    hospital deta moddling prac

---
+++ hospitalManagement/docotor.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: String,
+        required:true
+    },
+    qualification: {
+        type: String,
+        required: true
+    },
+    experienceInYears: {
+        type: Number,
+        default: 0
+    },
+    worksInHospital: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hospital"
+        }
+    ]
+}, {timestamps: ture})
+
+
+export const Doctor = mongoose.model("Doctor", doctorSchema)
